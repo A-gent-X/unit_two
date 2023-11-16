0c2f18a407910fbd54c64f9f697e8f4d19a33a64
Xavier Brown <xavieraustin1990@gmail>
2023-11-16 12:03:25 -0500

    unit two project

---
+++ MovieCard.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+
+const MovieCard = (props) => {
+  const {movie} = props
+  console.log(movie)
+  return (
+    <div className="movie-card">
+      <div>
+        <img src={`https://image.tmdb.org/t/p/orginal${movie.poster_path}`} />
+        <h3>{movie.original_title}</h3>
+      </div>
+      <button>Add to List</button>
+    </div>
+  )
+}
+
+export default MovieCard 
---
+++ MovieScreen.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import MovieCard from './MovieCard';
+
+const MovieScreen = (props) => {
+  const movieDisplay = props.movieList.map((movie, index) => {
+    return <MovieCard movie={movie}/>;
+  });
+
+  return (
+    <div className="page">
+      <h1> The X-hibit of Motion pictures</h1>
+      <h3>Add a movie to your watchlist!</h3>
+      <div className="movie-container">{movieDisplay}</div>
+    </div>
+  );
+};
+
+export default MovieScreen;
+ 
